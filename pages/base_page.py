da672a8aef0065a87882c58092a4911b0eb720dc
meekgot <ChrispeenLink@gmail.com>
2024-12-04 23:34:02 +0300

    first commit

---
+++ pages/base_page.py
@@ -0,0 +1,17 @@
+class BasePage:
+
+    def __init__(self, driver, base_url):
+        self.driver = driver
+        self.base_url = base_url
+
+    def visit(self):
+        return self.driver.get(self.base_url)
+
+    def get_url(self):
+        return self.driver.current_url
+
+    def equal_url(self):
+        if self.get_url() == self.base_url:
+            return True
+        else:
+            return False
---
+++ pages/demoqa.py
@@ -0,0 +1,21 @@
+from selenium.common.exceptions import NoSuchElementException
+from pages.base_page import BasePage
+from components.components import WebElement
+
+class DemoQa(BasePage):
+
+    def __init__(self, driver):
+        self.base_url = 'https://demoqa.com/'
+        super().__init__(driver, self.base_url)
+
+        self.icon = WebElement(driver, '#app > header > a')
+        self.btn_elements = WebElement(driver, '#app > div > div > div.home-body > div > div:nth-child(1)')
+        self.text_elements = WebElement(driver, 'footer')
+        self.text_class = WebElement(driver, '.col-12.mt-4.col-md-6')
+
+    def exist_icon(self):
+        try:
+            self.icon.find_element()
+        except NoSuchElementException:
+            return False
+        return True
