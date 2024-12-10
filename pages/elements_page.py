731d7b1226b8522f46714d1b469ad9c3aa3a5877
meekgot <ChrispeenLink@gmail.com>
2024-12-10 23:02:52 +0300

    first commit

--- pages/elements_page.py
+++ pages/elements_page.py
@@ -1,6 +1,14 @@
+from components.components import WebElement
 from pages.base_page import BasePage
 
 class ElementsPage(BasePage):
     def __init__(self, driver):
         self.base_url = 'https://demoqa.com/elements'
         super().__init__(driver, self.base_url)
+
+        self.text_elements = WebElement(driver, 'div.col-12:nth-child(2)')
+        self.icon = WebElement(driver, 'header > a > img')
+        self.btn_sidebar_first = WebElement(driver, 'div:nth-child(1) > span > div')
+        self.btn_sidebar_first_texbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-0 > span')
+        self.btn_sidebar_first_checkbox = WebElement(driver, 'div:nth-child(1) > div > ul > #item-1 > span')
+        self.btns_first_menu = WebElement(driver, 'div:nth-child(1) > div > ul > li')
--- tests/tests_hw/test_check_text.py
+++ tests/tests_hw/test_check_text.py
@@ -1,16 +1,30 @@
 from pages.demoqa import DemoQa
 from pages.elements_page import ElementsPage
+#
+# def test_check_text_footer(browser):
+#     demo_qa_page = DemoQa(browser)
+#     demo_qa_page.visit()
+#     current_text = '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
+#     assert demo_qa_page.text_elements.get_text() == current_text
+#
+#
+# def test_check_text(browser):
+#     demo_qa_page = DemoQa(browser)
+#     demo_qa_page.visit()
+#     demo_qa_page.btn_elements.click()
+#     current_text_elements = 'Please select an item from left to start practice.'
+#     assert demo_qa_page.text_class.get_text() == current_text_elements
 
-def test_check_text_footer(browser):
-    demo_qa_page = DemoQa(browser)
-    demo_qa_page.visit()
-    current_text = '© 2013-2020 TOOLSQA.COM | ALL RIGHTS RESERVED.'
-    assert demo_qa_page.text_elements.get_text() == current_text
+# def test_page_elements(browser):
+#     el_page = ElementsPage(browser)
+#
+#     el_page.visit()
+#     assert el_page.text_elements.get_text() == 'Please select an item from left to start practice.'
 
+def test_page_elements(browser):
+    el_page = ElementsPage(browser)
 
-def test_check_text(browser):
-    demo_qa_page = DemoQa(browser)
-    demo_qa_page.visit()
-    demo_qa_page.btn_elements.click()
-    current_text_elements = 'Please select an item from left to start practice.'
-    assert demo_qa_page.text_class.get_text() == current_text_elements
+    el_page.visit()
+    assert el_page.icon.exist()
+    assert el_page.btn_sidebar_first.exist()
+    assert el_page.btn_sidebar_first_texbox.exist()
